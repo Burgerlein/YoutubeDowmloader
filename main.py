b738cbb07538ccb85a79174975024bf9b6ac1ceb
Tim <tim.rosenbach@icloud.com>
2024-01-31 16:12:20 +0100

    removed menu

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from pytube import YouTube
-from pick import pick
 
-title = 'Please Choose an option where to install the video: '
-options = ['Desktop', 'Dowmloads Folder']
-option, index = pick(options, title, indicator='=>', default_index=0)
 
 def Download(link):
     youtubeObject = YouTube(link)
