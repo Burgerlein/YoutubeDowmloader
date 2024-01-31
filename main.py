81a6fdfd8db83bba8d0ab57ae69bc1272180d4ab
Tim <tim.rosenbach@icloud.com>
2024-01-31 13:31:08 +0100

    add a menu to choose where dowmload to save

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pytube import YouTube
 from pick import pick
 
+title = 'Please Choose an option where to install the video: '
+options = ['Desktop', 'Dowmloads Folder']
+option, index = pick(options, title, indicator='=>', default_index=0)
 
 link = input("Geben sie den Youtube Link ein: ")
