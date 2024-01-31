7dfda300545faac163478e24f937e609e1743feb
Tim <tim.rosenbach@icloud.com>
2024-01-31 15:05:22 +0100

    rebuild download function

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ title = 'Please Choose an option where to install the video: '
 options = ['Desktop', 'Dowmloads Folder']
 option, index = pick(options, title, indicator='=>', default_index=0)
 
-link = input("Geben sie den Youtube Link ein: ")
+def Download(link):
+    youtubeObject = YouTube(link)
+    try:
+        video = youtubeObject.streams.get_highest_resolution()
+        video.download()
+
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+link = str(input("Geben sie den Youtube Link ein: "))
+Download(link)
